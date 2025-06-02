b4c198a53c9469c5da1f2db218ebae702c2fba27
Carlo Siniscalchi <carlo.siniscalchi.03@gmail.com>
2025-06-02 18:29:26 +0200

    import libraries

--- main.R
+++ main.R
@@ -1,1 +1,27 @@
-print("test")
+Alessandro
+
+18:21 (0 minuti fa)
+
+a me
+install.packages(c(
+  "openxlsx",
+  "car",
+  "BAS",
+  "rgl",
+  "MASS",
+  "leaps",
+  "GGally",
+  "ellipse",
+  "faraway",
+  "corrplot"
+))
+
+library(car)
+library(BAS)
+library(rgl)
+library(MASS)
+library(leaps)
+library(GGally)
+library(ellipse)
+library(faraway)
+library(corrplot)
